f1c29c49daf9bfff7400e4e62daadd9b83ba943e
OCHOA <OCHOA@Gilberto>
2024-02-12 20:41:37 -0600

    Change: Delete some lines in IDE.java y puse "Hola"

--- src/compilador/IDE.java
+++ src/compilador/IDE.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package compilador;
+//Hola 
 
-/**
- *
- * @author gmva2
- */
 public class IDE extends javax.swing.JFrame {
     
     NumeroLinea numerolinea;
